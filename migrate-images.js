272fea33bb47064cdb62034bca6711b84190b8ff
seyhancanyakan <seyhancanyakan@hotmail.com>
2025-11-18 23:56:57 +0300

    Fix API async issues - await createClient()
    Fixed:
    - Profile API
    - Jobs API
    - Jobs create API
    
    Supabase client properly awaited!

---
+++ migrate-images.js
@@ -0,0 +1,4 @@
+// Migration script - run once to move existing images
+console.log("Migration: Move ComfyUI images to Supabase");
+console.log("Login to site first, then run this script");
+console.log("Images will be associated with your account");
